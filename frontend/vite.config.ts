5137cbf1a24f264c8450a9ba80938e4608caab35
Venkata Lokesh <venkatalokeshvl@gmail.com>
2024-06-27 19:30:33 +0530

    changes for production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import envCompatible from "vite-plugin-env-compatible"
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 8000,
-    },
+  // envPrefix: "REACT_APP_",
+  plugins: [react(), envCompatible()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
