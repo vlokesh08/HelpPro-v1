3d32463c7c47d6282c8f200e26cbaf3a086c19a5
Venkata Lokesh <venkatalokeshvl@gmail.com>
2024-07-09 12:42:19 +0530

    profile

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,29 +1,7 @@
-import Navbar from '@/components/Navbar/Navbar'
-import Temp from './Temp'
-import { useEffect, useState } from 'react';
-import HomeScreenLoading from '@/components/LoadingPages/HomeScreenLoading';
 
 const ProfilePage = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (isLoading) {
-    return <div>
-      <HomeScreenLoading />
-    </div>;
-  }
   return (
-    <div>
-        <Navbar />
-        <Temp />
-    </div>
+    <div>ProfilePage</div>
   )
 }
 
